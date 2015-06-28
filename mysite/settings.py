1064ed358c1d33f50b60fa18a9bfb0b7b52624da
Kevin Yokley <kyokley2@gmail.com>
2015-06-28 07:15:06 -0500

    Restricting API access to admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,9 +149,10 @@ INSTALLED_APPS = (
 )
 
 REST_FRAMEWORK = {
-        'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticatedOrReadOnly',),
+        'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
         'DEFAULT_AUTHENTICATION_CLASSES': (
             'rest_framework.authentication.BasicAuthentication',
+            'rest_framework.authentication.SessionAuthentication',
             ),
         'PAGINATE_BY': 10,
 }
