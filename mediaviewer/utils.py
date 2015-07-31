5fd6e622633d14718c229678cf5360a476f8eea1
Kevin Yokley <kyokley2@gmail.com>
2015-07-31 11:16:33 -0500

    Adding json response decorator

--- mediaviewer/utils.py
+++ mediaviewer/utils.py
@@ -1,8 +1,9 @@
-import os
+import os, json
 from mysite.settings import LOG_ACCESS_TIMINGS
 from datetime import datetime
 from binascii import hexlify
 from functools import wraps
+from django.http import HttpResponse
 
 from mediaviewer.log import log
 
@@ -46,3 +47,24 @@ def logAccessInfo(func):
 
         return res
     return wrap
+
+def json_response(func):
+    """
+    A decorator thats takes a view response and turns it
+    into json. If a callback is added through GET or POST
+    the response is JSONP.
+    """
+    def decorator(request, *args, **kwargs):
+        objects = func(request, *args, **kwargs)
+        if isinstance(objects, HttpResponse):
+            return objects
+        try:
+            data = json.dumps(objects)
+            if 'callback' in request.REQUEST:
+                # a jsonp response!
+                data = '%s(%s);' % (request.REQUEST['callback'], data)
+                return HttpResponse(data, "text/javascript")
+        except:
+            data = json.dumps(str(objects))
+        return HttpResponse(data, "application/json")
+    return decorator
