599fc161ee99fe098262a9a16e7e7889f58871bf
Kevin Yokley <kyokley2@gmail.com>
2017-04-22 09:13:08 -0500

    Making docker progress

--- Dockerfile
+++ Dockerfile
@@ -2,26 +2,37 @@ FROM alpine:latest
 
 MAINTAINER Kevin Yokley
 
-# Install required packages and remove the apt packages cache when done.
+ENV HOME /home/docker
+RUN mkdir $HOME
+RUN addgroup -S docker && \
+    adduser -S docker -g docker \
+    && chown -R docker:docker $HOME
 
+# Install required packages and remove the apt packages cache when done.
 RUN apk update
 RUN apk add --no-cache --virtual .build-deps \
     linux-headers \
     g++ \
-	git \
-	python3 \
-	python3-dev \
-	nginx \
-	supervisor \
+    git \
+    python3 \
+    python3-dev \
+    nginx \
+    supervisor \
+    postgresql \
+    postgresql-dev \
     nodejs && \
-	pip3 install -U pip setuptools
+    pip3 install -U pip \
+                    setuptools
 
 RUN pip3 install uwsgi django
 
 # setup all the configfiles
+#VOLUME nginx
 RUN echo "daemon off;" >> /etc/nginx/nginx.conf
-COPY nginx-app.conf /etc/nginx/sites-available/default
-COPY supervisor-app.conf /etc/supervisor/conf.d/
+
+#VOLUME /etc/supervisor.d/supervisor.conf
+#RUN mkdir /etc/supervisor.d
+#RUN ln -s /home/docker/code/supervisor/supervisor.conf /etc/supervisor.d/supervisor.conf
 
 # COPY requirements.txt and RUN pip install BEFORE adding the rest of your code, this will cause Docker's caching mechanism
 # to prevent re-installing (all your) dependencies when you made a change a line or two in your app.
@@ -30,7 +41,18 @@ COPY requirements.txt /home/docker/code/app/
 RUN pip3 install -r /home/docker/code/app/requirements.txt
 
 # add (the rest of) our code
-COPY . /home/docker/code/
+COPY . $HOME/code/
+
+RUN ln -s /home/docker/code/nginx/nginx-mediaviewer.conf /etc/nginx/conf.d/mediaviewer.conf
+#RUN ln -s /home/docker/code/supervisor/supervisor.conf /etc/supervisor.d/supervisor.conf
+
+RUN rm -f $HOME/code/mysite/local_settings.py && \
+    cp $HOME/code/mysite/docker_settings.py $HOME/code/mysite/local_settings.py
+
+
+WORKDIR $HOME
+#USER docker
 
 EXPOSE 8000 8001 8002
-CMD ["supervisord", "-n"]
+#ENTRYPOINT ["supervisord", "-n", "-c", "/etc/supervisor.d/supervisor.conf"]
+CMD /bin/sh
---
+++ mysite/docker_settings.py
@@ -0,0 +1,64 @@
+import os
+
+DEBUG = False
+
+ADMINS = (
+     ('Docker', 'docker@example.com'),
+)
+MANAGERS = ADMINS
+
+DATABASES = {
+    # Testing settings!!!
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mediaviewer',                      # Or path to database file if using sqlite3.
+        'USER': 'docker',
+        'PASSWORD': 'password',
+        'HOST': 'postgres',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    },
+}
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'mediaviewer.middleware.AutoLogout',
+    #'axes.middleware.FailedLoginMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'axes',
+    # Uncomment the next line to enable the admin:
+    'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    'django.contrib.admindocs',
+    'widget_tweaks',
+     #'django_memcached',
+    'rest_framework',
+    'djangobower',
+    'mediaviewer',
+    'mediaviewer.models',
+    'mediaviewer.views',
+)
+
+WAITER_STATUS_URL = 'http://127.0.0.1:5000/waiter/status'
+WAITER_HEAD = 'http://'
+BANGUP_WAITER_IP_FORMAT_MOVIES = '127.0.0.1:5000/waiter/dir/'
+BANGUP_WAITER_IP_FORMAT_TVSHOWS = '127.0.0.1:5000/waiter/file/'
+
+BYPASS_SMTPD_CHECK = True
+
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+os.environ['HTTPS'] = 'off'
+os.environ['wsgi.url_scheme'] = 'http'
