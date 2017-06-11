6956c4a4b73ab02c314205a656004cfe591524a2
Kevin Yokley <kyokley2@gmail.com>
2017-06-11 18:34:17 -0500

    Returning 500 for exceptions

--- mediaviewer/views/comment.py
+++ mediaviewer/views/comment.py
@@ -17,43 +17,37 @@ from mediaviewer.utils import logAccessInfo, check_force_password_change
 def comment(request, file_id):
     file = get_object_or_404(File, pk=file_id)
 
-    try:
-        comment = request.POST.get('comment')
-        viewed = request.POST.get('viewed') == 'true' and True or False
-        usercomment = file.usercomment(request.user)
-        if usercomment:
-            usercomment.comment = comment
-            usercomment.viewed = viewed
-        else:
-            UserComment.new(file,
-                            request.user,
-                            comment,
-                            viewed)
-
-        if request.user.is_staff:
-            if (file._searchString != request.POST.get('search', file._searchString) or
-                    file.imdb_id != request.POST.get('imdb_id', file.imdb_id) or
-                    file.override_filename != request.POST.get('episode_name', file.override_filename) or
-                    file.override_season != request.POST.get('season', file.override_season) or
-                    file.override_episode != request.POST.get('episode_number', file.override_episode)):
-                file.posterfile.delete()
-                if file.isTVShow() and file.path.posterfile:
-                    file.path.posterfile.delete()
-                file._searchString = request.POST.get('search', file._searchString)
-                file.imdb_id = request.POST.get('imdb_id', file.imdb_id)
-                file.override_filename = request.POST.get('episode_name', file.override_filename)
-                file.override_season = request.POST.get('season', file.override_season)
-                file.override_episode = request.POST.get('episode_number', file.override_episode)
-
-            file.hide = request.POST.get('hidden', file.hide) == 'true' or False
-            file.save()
-
-    except Exception:
-        return render(request, 'mediaviewer/filesdetail.html',
-                {'file': file,
-                 'error_message': 'An error has occurred',})
+    comment = request.POST.get('comment')
+    viewed = request.POST.get('viewed') == 'true' and True or False
+    usercomment = file.usercomment(request.user)
+    if usercomment:
+        usercomment.comment = comment
+        usercomment.viewed = viewed
     else:
-        return HttpResponseRedirect(reverse('mediaviewer:results', args=(file.id,)))
+        UserComment.new(file,
+                        request.user,
+                        comment,
+                        viewed)
+
+    if request.user.is_staff:
+        if (file._searchString != request.POST.get('search', file._searchString) or
+                file.imdb_id != request.POST.get('imdb_id', file.imdb_id) or
+                file.override_filename != request.POST.get('episode_name', file.override_filename) or
+                file.override_season != request.POST.get('season', file.override_season) or
+                file.override_episode != request.POST.get('episode_number', file.override_episode)):
+            file.posterfile.delete()
+            if file.isTVShow() and file.path.posterfile:
+                file.path.posterfile.delete()
+            file._searchString = request.POST.get('search', file._searchString)
+            file.imdb_id = request.POST.get('imdb_id', file.imdb_id)
+            file.override_filename = request.POST.get('episode_name', file.override_filename)
+            file.override_season = request.POST.get('season', file.override_season)
+            file.override_episode = request.POST.get('episode_number', file.override_episode)
+
+        file.hide = request.POST.get('hidden', file.hide) == 'true' or False
+        file.save()
+
+    return HttpResponseRedirect(reverse('mediaviewer:results', args=(file.id,)))
 
 @login_required(login_url='/mediaviewer/login/')
 @check_force_password_change
