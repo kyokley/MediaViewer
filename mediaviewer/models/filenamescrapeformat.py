20987ff2984adf71e27889b57f996aecfbea2ad1
Kevin Yokley <kyokley2@gmail.com>
2018-04-28 13:23:33 -0500

    Implementing functionality to find path from filename

--- mediaviewer/models/filenamescrapeformat.py
+++ mediaviewer/models/filenamescrapeformat.py
@@ -1,3 +1,4 @@
+import re
 from django.db import models
 
 class FilenameScrapeFormat(models.Model):
@@ -14,3 +15,72 @@ class FilenameScrapeFormat(models.Model):
     def __unicode__(self):
         return 'id: %s n: %s s: %s e: %s subPeriods: %s useSearchTerm: %s' % (self.id, self.nameRegex, self.seasonRegex, self.episodeRegex, self.subPeriods, self.useSearchTerm)
 
+    @classmethod
+    def new(cls,
+            nameRegex,
+            seasonRegex,
+            episodeRegex,
+            subPeriods=False,
+            useSearchTerm=False):
+        obj = cls()
+        obj.nameRegex = nameRegex
+        obj.seasonRegex = seasonRegex
+        obj.episodeRegex = episodeRegex
+        obj.subPeriods = subPeriods
+        obj.useSearchTerm = useSearchTerm
+        obj.save()
+        return obj
+
+    def valid_for_filename(self, filename):
+        from mediaviewer.models.path import Path
+
+        path = None
+
+        if self.subPeriods:
+            filename = filename.replace('.', ' ')
+
+        name = re.findall(self.nameRegex, filename)
+
+        if not name:
+            return None
+        else:
+            name = name[0]
+
+            for p in Path.objects.filter(is_movie=False).order_by('-id'):
+                if name.strip().lower() in p.displayName().lower():
+                    path = p
+                    break
+            else:
+                return None
+
+        season = re.findall(self.seasonRegex, filename)
+        if not season:
+            return None
+        else:
+            season = season[0]
+
+            if not season.isdigit():
+                return None
+
+        episode = re.findall(self.episodeRegex, filename)
+        if not episode:
+            return None
+        else:
+            episode = episode[0]
+
+            if not episode.isdigit():
+                return None
+
+        return path
+
+    @classmethod
+    def path_for_filename(cls, filename):
+        paths = []
+
+        for scraper in FilenameScrapeFormat.objects.all():
+            path = scraper.valid_for_filename(filename)
+            if path:
+                paths.append(path)
+
+        paths.sort(key=lambda x: x.id, reverse=True)
+        return paths[0] if paths else None
--- mediaviewer/tests/models/test_filenamescrapeformat.py
+++ mediaviewer/tests/models/test_filenamescrapeformat.py
@@ -46,3 +46,50 @@ class TestValidForFilename(TestCase):
         actual = self.scraper.valid_for_filename(test_filename)
 
         self.assertEqual(expected, actual)
+
+class TestPathForFilename(TestCase):
+    def setUp(self):
+        self.path1 = Path.new('/path/to/local',
+                              '/path/to/remote',
+                              is_movie=False)
+        self.path1.override_display_name = 'Foo Is Bar'
+        self.path1.save()
+
+        self.path2 = Path.new('/path/to/local2',
+                              '/path/to/remote2',
+                              is_movie=False)
+        self.path2.override_display_name = 'Foo Is Not Bar'
+        self.path2.save()
+
+        self.path3 = Path.new('/path/to/local3',
+                              '/path/to/remote3',
+                              is_movie=False)
+        self.path3.override_display_name = 'Boo As Baz'
+        self.path3.save()
+
+        self.path4 = Path.new('/path/to/local',
+                              '/path/to/remote4',
+                              is_movie=False)
+        self.path4.override_display_name = 'Foo Is Bar'
+        self.path4.save()
+
+        self.scraper = FilenameScrapeFormat.new(nameRegex=r'[fF]oo[\.\s\-][Ii]s[\.\s\-][Bb]ar',
+                                                seasonRegex=r'(?<=[sS])\d{2}',
+                                                episodeRegex=r'(?<=[eE])\d{2}',
+                                                subPeriods=True)
+
+    def test_found(self):
+        test_filename = 'Foo.is.bar.S02E01.mpg'
+
+        expected = self.path4
+        actual = FilenameScrapeFormat.path_for_filename(test_filename)
+
+        self.assertEqual(expected, actual)
+
+    def test_not_found(self):
+        test_filename = 'Foo.is.not.bar.mpg'
+
+        expected = None
+        actual = FilenameScrapeFormat.path_for_filename(test_filename)
+
+        self.assertEqual(expected, actual)
