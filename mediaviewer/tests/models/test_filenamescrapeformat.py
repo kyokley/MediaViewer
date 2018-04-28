3d33d326bba1e3f3df3ec9dc9d51344ffc7f0e64
Kevin Yokley <kyokley2@gmail.com>
2018-04-28 12:45:36 -0500

    Adding tests for determining path by filename

--- mediaviewer/tests/models/test_filenamescrapeformat.py
+++ mediaviewer/tests/models/test_filenamescrapeformat.py
@@ -4,19 +4,45 @@ from mediaviewer.models.path import Path
 
 class TestValidForFilename(TestCase):
     def setUp(self):
-        self.path = Path()
+        self.path = Path.new('/path/to/local',
+                             '/path/to/remote',
+                             is_movie=False)
         self.path.override_display_name = 'Foo Is Bar'
+        self.path.save()
 
         self.scraper = FilenameScrapeFormat.new(nameRegex=r'[fF]oo[\.\s\-][Ii]s[\.\s\-][Bb]ar',
                                                 seasonRegex=r'(?<=[sS])\d{2}',
-                                                episodeRegex=r'(?<=[eE])\d{2}')
+                                                episodeRegex=r'(?<=[eE])\d{2}',
+                                                subPeriods=True)
 
     def test_is_valid(self):
         test_filename = 'Foo.is.bar.S02E01.mpg'
 
-        self.assertTrue(FilenameScrapeFormat.valid_for_filename(test_filename))
+        expected = self.path
+        actual = self.scraper.valid_for_filename(test_filename)
 
-    def test_is_not_valid(self):
+        self.assertEqual(expected, actual)
+
+    def test_name_not_valid(self):
         test_filename = 'Foo.is.not.bar.S02E01.mpg'
 
-        self.assertFalse(FilenameScrapeFormat.valid_for_filename(test_filename))
+        expected = None
+        actual = self.scraper.valid_for_filename(test_filename)
+
+        self.assertEqual(expected, actual)
+
+    def test_season_and_episode_not_valid(self):
+        test_filename = 'Foo.is.not.bar.201.mpg'
+
+        expected = None
+        actual = self.scraper.valid_for_filename(test_filename)
+
+        self.assertEqual(expected, actual)
+
+    def test_no_season_not_valid(self):
+        test_filename = 'Foo.is.not.bar.mpg'
+
+        expected = None
+        actual = self.scraper.valid_for_filename(test_filename)
+
+        self.assertEqual(expected, actual)
