83291243d824aebb5846be814ba2669882735303
Kevin Yokley <kyokley2@gmail.com>
2018-11-07 09:18:26 -0600

    Add test for waiterstatus

--- mediaviewer/models/waiterstatus.py
+++ mediaviewer/models/waiterstatus.py
@@ -1,16 +1,23 @@
 from django.db import models
 
+
 class WaiterStatus(models.Model):
     status = models.BooleanField(db_column='status')
     failureReason = models.TextField(db_column='failurereason')
-    datecreated = models.DateTimeField(db_column='datecreated', auto_now_add=True)
+    datecreated = models.DateTimeField(
+            db_column='datecreated',
+            auto_now_add=True)
 
     class Meta:
         app_label = 'mediaviewer'
         db_table = 'waiterstatus'
 
     def __unicode__(self):
-        return 'id: %s status: %s failureReason: %s date: %s' % (self.id, self.status, self.failureReason, self.datecreated)
+        return 'id: %s status: %s failureReason: %s date: %s' % (
+                self.id,
+                self.status,
+                self.failureReason,
+                self.datecreated)
 
     @classmethod
     def new(cls,
@@ -27,4 +34,3 @@ class WaiterStatus(models.Model):
     def getLastStatus(cls):
         obj = WaiterStatus.objects.order_by('-id')
         return obj and obj[0] or None
-
---
+++ mediaviewer/tests/models/test_waiterstatus.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+from mediaviewer.models.waiterstatus import WaiterStatus
+
+
+class TestNew(TestCase):
+    def test_new(self):
+        ws = WaiterStatus.new(True, 'reason')
+
+        self.assertEqual(ws.status, True)
+        self.assertEqual(ws.failureReason, 'reason')
+
+
+class TestGetLastStatus(TestCase):
+    def setUp(self):
+        self.ws1 = WaiterStatus.new(False, 'reason1')
+        self.ws2 = WaiterStatus.new(False, 'reason2')
+
+    def test_getLastStatus(self):
+        ws = WaiterStatus.getLastStatus()
+        self.assertEqual(ws, self.ws2)
