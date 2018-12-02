22bbf18f90018644acbd8d2a28d9ba64bdd6a375
Kevin Yokley <kyokley2@gmail.com>
2018-12-02 16:55:27 -0600

    Update docker config

--- configs/docker_settings.py
+++ configs/docker_settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     # Testing settings!!!
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mediaviewer',                      # Or path to database file if using sqlite3.
-        'USER': 'docker',
+        'NAME': 'postgres',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',
         'PASSWORD': 'password',
         'HOST': 'postgres',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
