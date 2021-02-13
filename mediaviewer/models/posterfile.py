2f2c684f96386d90049bbbf19724d687d75066f9
Kevin Yokley <kyokley2@gmail.com>
2021-02-13 11:21:00 -0600

    Fix property

--- mediaviewer/models/posterfile.py
+++ mediaviewer/models/posterfile.py
@@ -61,7 +61,7 @@ class PosterFile(models.Model):
 
     @property
     def filename(self):
-        return self.file and self.file.displayName
+        return self.file and self.file.displayName()
 
     @property
     def pathname(self):
