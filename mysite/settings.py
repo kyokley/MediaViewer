0bdabd0b3fae3edebdfd7d90063f52e18571445d
Kevin Yokley <kyokley2@gmail.com>
2021-03-03 08:13:58 -0600

    Increase DRF pagination

--- mysite/settings.py
+++ mysite/settings.py
@@ -164,7 +164,7 @@ REST_FRAMEWORK = {
             ),
         'DEFAULT_PAGINATION_CLASS': (
             'rest_framework.pagination.PageNumberPagination'),
-        'PAGE_SIZE': 30,
+        'PAGE_SIZE': 100,
 }
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
