1b64bc10130ff9bd3e281ee9b5dc55d649a224cf
Kevin Yokley <kyokley2@gmail.com>
2023-05-07 17:30:59 -0500

    Remove config check

--- mediaviewer/apps.py
+++ mediaviewer/apps.py
@@ -1,16 +1,6 @@
 from django.apps import AppConfig
-from django.conf import settings
 
 
 class MediaviewerConfig(AppConfig):
     name = "mediaviewer"
     default_auto_field = "django.db.models.BigAutoField"
-
-    def ready(self):
-        if not settings.DEBUG:
-            if not (settings.AUTH0_CLIENT_ID and
-                    settings.AUTH0_CLIENT_SECRET and
-                    settings.AUTH0_DOMAIN and
-                    settings.AUTH0_LOGIN and
-                    settings.AUTH0_PASSWORD):
-                raise ValueError('Improper configuration')
