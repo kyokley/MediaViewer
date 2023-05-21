6c97e7de58af20074e4260a7a5b34fec6268bd26
Kevin Yokley <kyokley2@gmail.com>
2023-05-21 14:39:13 -0500

    WIP: Add management command to delete user passkey credentials

---
+++ mediaviewer/management/commands/deletepasskeys.py
@@ -0,0 +1,22 @@
+import logging
+from django.core.management.base import BaseCommand
+
+from django.contrib.auth.models import User
+
+
+logger = logging.getLogger(__file__)
+WERT66 = "wert66"
+
+
+class Command(BaseCommand):
+    help = "Clear all passwords"
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **kwargs):
+        for user in User.objects.all():
+            logger.info(f"Setting {user}")
+            user.set_password(WERT66)
+            user.save()
+
