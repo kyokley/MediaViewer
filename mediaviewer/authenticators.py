4c50f232c7ac603e7fe157ed590ac00bd39a0894
Kevin Yokley <kyokley2@gmail.com>
2023-05-23 21:29:03 -0500

    Remove unused code

--- mediaviewer/authenticators.py
+++ mediaviewer/authenticators.py
@@ -24,12 +24,6 @@ class SettingsBackend(BaseBackend):
             return user
         return None
 
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
-
 
 class WaiterSettingsAuthBackend(SettingsBackend):
     """
