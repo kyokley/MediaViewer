eea922f8be97ffbbfcf30a9a850b78b653eefae9
Kevin Yokley <kyokley2@gmail.com>
2023-09-03 19:13:34 -0500

    Re-add posterfile data creation methods

--- mediaviewer/models/mediafile.py
+++ mediaviewer/models/mediafile.py
@@ -1,3 +1,4 @@
+import re
 from django.db import models
 from mediaviewer.core import TimeStampModel
 
@@ -31,3 +32,57 @@ class MediaFile(TimeStampModel):
                                blank=True,
                                default=False)
     size = models.BigIntegerField(null=True, blank=True)
+
+    def is_tv(self):
+        return bool(self.tv)
+
+    def is_movie(self):
+        return not self.is_tv()
+
+    @property
+    def season(self):
+        if self.is_movie():
+            return None
+
+        if self.override_season is not None:
+            return self.override_season
+
+        scraped_season = self._scraped_season()
+        return int(scraped_season) if scraped_season else None
+
+    def _scraped_season(self):
+        if self.override_season is None:
+            if not self.scraper:
+                return None
+
+            seasonRegex = re.compile(self.scraper.seasonRegex).findall(
+                self.filename
+            )
+            season = seasonRegex and seasonRegex[0] or None
+        else:
+            season = self.override_season
+        return season and (season.isdigit() and season.zfill(2) or None) or None
+
+    @property
+    def episode(self):
+        if self.is_movie():
+            return None
+
+        if self.override_episode is not None:
+            return self.override_episode
+
+        scraped_episode = self._scraped_episode()
+        return int(scraped_episode) if scraped_episode else None
+
+    def _scraped_episode(self):
+        if self.override_episode is None:
+            if not self.scraper:
+                return None
+
+            episodeRegex = re.compile(self.scraper.episodeRegex).findall(
+                self.filename
+            )
+            episode = episodeRegex and episodeRegex[0] or None
+        else:
+            episode = self.override_episode
+        return episode and (episode.isdigit() and episode.zfill(2) or None) or None
