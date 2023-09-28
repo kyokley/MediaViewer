6bab0283b8448888628662e35ca65cfc00d18977
Kevin Yokley <kyokley2@gmail.com>
2023-09-28 13:55:34 -0500

    Remove unused code

--- mediaviewer/views/files.py
+++ mediaviewer/views/files.py
@@ -1,16 +1,9 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import (
-    render,
     get_object_or_404,
 )
 
 from mediaviewer.models.file import File
-from mediaviewer.views.views_utils import setSiteWideContext
-from django.contrib.auth.decorators import login_required
-from mediaviewer.models.usersettings import (
-    LOCAL_IP,
-    BANGUP_IP,
-)
 from django.contrib.auth.models import User
 from mediaviewer.models.message import Message
 from django.contrib import messages
@@ -21,33 +14,6 @@ import json
 import re
 
 ID_REGEX = re.compile(r"\d+")
-
-
-@login_required(login_url="/mediaviewer/login/")
-@logAccessInfo
-def files(request, items):
-    user = request.user
-    items = int(items)
-    if items:
-        files = File.objects.order_by("-id")[:items]
-    else:
-        files = File.objects.order_by("-id")
-    files = files.select_related("path")
-
-    settings = user.settings()
-    context = {
-        "items": items,
-        "view": "files",
-        "LOCAL_IP": LOCAL_IP,
-        "BANGUP_IP": BANGUP_IP,
-        "can_download": (settings and settings.can_download or False),
-        "jump_to_last": (settings and settings.jump_to_last_watched or False),
-        "table_data_page": "ajaxfilesrows",
-    }
-    context["active_page"] = "files"
-    context["title"] = "Files"
-    setSiteWideContext(context, request, includeMessages=True)
-    return render(request, "mediaviewer/files.html", context)
 
 
 @logAccessInfo
