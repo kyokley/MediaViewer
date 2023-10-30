338b3fa9619718286db3883bf8b072de2f4846c5
Kevin Yokley <kyokley2@gmail.com>
2023-10-29 21:44:44 -0500

    Refactor most popular genres

--- mediaviewer/models/genre.py
+++ mediaviewer/models/genre.py
@@ -1,10 +1,40 @@
 from django.db import models
 
 
+class GenreManager(models.Manager):
+    def get_movie_genres(self, limit=10):
+        genres = self.filter(
+            pk__in=(
+                self.filter(poster__movie__isnull=False)
+                .values("id")
+                .annotate(genre_count=models.Count("poster__movie"))
+                .order_by("-genre_count", "genre")
+                .values("id")[:limit]
+            )
+        ).order_by("genre")
+
+        return genres
+
+    def get_tv_genres(self, limit=10):
+        genres = self.filter(
+            pk__in=(
+                self.filter(poster__tv__isnull=False)
+                .values("id")
+                .annotate(genre_count=models.Count("poster__tv"))
+                .order_by("-genre_count")
+                .values("id")[:limit]
+            )
+        ).order_by("genre")
+
+        return genres
+
+
 class Genre(models.Model):
     genre = models.TextField(blank=False, null=False)
     datecreated = models.DateTimeField(auto_now_add=True)
     dateedited = models.DateTimeField(auto_now=True)
+
+    objects = GenreManager()
 
     class Meta:
         app_label = "mediaviewer"
@@ -24,31 +54,3 @@ class Genre(models.Model):
         new_obj.genre = genre.title()
         new_obj.save()
         return new_obj
-
-    @classmethod
-    def get_movie_genres(cls, limit=10):
-        genres = cls.objects.filter(
-            pk__in=(
-                cls.objects.filter(poster__movie__isnull=False)
-                .values("id")
-                .annotate(genre_count=models.Count("poster__movie"))
-                .order_by("-genre_count", "genre")
-                .values("id")[:limit]
-            )
-        ).order_by("genre")
-
-        return genres
-
-    @classmethod
-    def get_tv_genres(cls, limit=10):
-        genres = cls.objects.filter(
-            pk__in=(
-                cls.objects.filter(poster__tv__isnull=False)
-                .values("id")
-                .annotate(genre_count=models.Count("poster__tv"))
-                .order_by("-genre_count")
-                .values("id")[:limit]
-            )
-        ).order_by("genre")
-
-        return genres
