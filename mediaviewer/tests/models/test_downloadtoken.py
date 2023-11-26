711e44ceba7e2e4836d8ed09832d175357389770
Kevin Yokley <kyokley2@gmail.com>
2023-11-26 17:33:36 -0600

    Update DownloadToken tests

--- mediaviewer/tests/models/test_downloadtoken.py
+++ mediaviewer/tests/models/test_downloadtoken.py
@@ -1,63 +1,39 @@
-import mock
 import pytest
 
-from mediaviewer.tests import helpers
+from datetime import timedelta
 from mediaviewer.models.downloadtoken import DownloadToken
-from mediaviewer.models.file import File
-from mediaviewer.models.path import Path
 from django.conf import settings
 
 
 @pytest.mark.django_db
-class TestDownloadToken:
-    @pytest.fixture(autouse=True)
-    def setUp(self, mocker):
-        self.mock_save = mocker.patch(
-            "mediaviewer.models.downloadtoken.DownloadToken.save"
-        )
+@pytest.mark.parametrize(
+    'use_movie', (True, False))
+class TestIsValid:
+    @pytest.fixture
+    def setUp(self,
+              create_tv_media_file,
+              create_movie_media_file,
+              create_user):
+        self.user = create_user()
 
-        self.mock_objects = mocker.patch(
-            "mediaviewer.models.downloadtoken.DownloadToken.objects"
-        )
+        self.tv_mf = create_tv_media_file()
+        self.movie_mf = create_movie_media_file()
 
-        self.mock_createLastWatchedMessage = mocker.patch(
-            "mediaviewer.models.downloadtoken.Message.createLastWatchedMessage"
-        )
+    def test_is_valid(self, use_movie):
+        if use_movie:
+            mf = self.movie_mf
+        else:
+            mf = self.tv_mf
 
-        self.user = helpers.create_user()
-
-        self.path = Path.objects.create(
-            localpathstr="/path/to/file",
-            remotepathstr="/path/to/file",
-            is_movie=False,
-        )
-
-        self.file = File.new(
-            "some file",
-            self.path,
-        )
-
-    def test_new(self):
-        self.mock_objects.count.return_value = (
-            settings.MAXIMUM_NUMBER_OF_STORED_DOWNLOAD_TOKENS + 1
-        )
-        mock_ordered_query = mock.MagicMock()
-        old_token = mock.create_autospec(DownloadToken)
-        mock_ordered_query.first.return_value = old_token
-        self.mock_objects.order_by.return_value = mock_ordered_query
-        dt = DownloadToken.new(self.user, self.file)
-
-        assert dt.filename == "some file"
-        assert dt.path == "/path/to/file"
-        assert not dt.ismovie
-        assert dt.displayname == "some file"
-        assert dt.file == self.file
-        self.mock_save.assert_called_once_with()
-        assert self.mock_objects.count.called
-        assert old_token.delete.called
-        self.mock_createLastWatchedMessage.assert_called_once_with(self.user, self.file)
-
-    def test_isvalid(self):
-        self.mock_objects.count.return_value = 1
-        dt = DownloadToken.new(self.user, self.file)
+        dt = DownloadToken.objects.from_media_file(self.user, mf)
         assert dt.isvalid
+
+    def test_is_invalid(self, use_movie):
+        if use_movie:
+            mf = self.movie_mf
+        else:
+            mf = self.tv_mf
+
+        dt = DownloadToken.objects.from_media_file(self.user, mf)
+        dt.date_created = dt.date_created - timedelta(hours=settings.TOKEN_VALIDITY_LENGTH, seconds=1)
+        assert not dt.isvalid
