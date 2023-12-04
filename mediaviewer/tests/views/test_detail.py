b2f6abf8ef40f5eae94773ca38db7c4658aabf05
Kevin Yokley <kyokley2@gmail.com>
2023-12-03 18:05:53 -0600

    Use fancy new fixtures

--- mediaviewer/tests/views/test_detail.py
+++ mediaviewer/tests/views/test_detail.py
@@ -7,23 +7,13 @@ from mediaviewer.views.detail import (
     ajaxdownloadbutton,
     ajaxsuperviewed,
     ajaxviewed,
-    filesdetail,
-    downloadlink,
     autoplaydownloadlink,
 )
 from django.contrib.auth.models import (
     Group,
     AnonymousUser,
 )
-from mediaviewer.models.usersettings import (
-    UserSettings,
-    LOCAL_IP,
-    BANGUP_IP,
-)
 from mediaviewer.models.downloadtoken import DownloadToken
-from mediaviewer.models.file import File
-from mediaviewer.models.path import Path
-from mediaviewer.models.usercomment import UserComment
 
 
 @pytest.mark.django_db
@@ -158,120 +148,22 @@ class TestAjaxSuperViewedResponseStatusCode:
 
 
 @pytest.mark.django_db
-class TestFilesDetail:
-    @pytest.fixture(autouse=True)
-    def setUp(self, mocker):
-        self.mock_setSiteWideContext = mocker.patch(
-            "mediaviewer.views.detail.setSiteWideContext"
-        )
-
-        self.mock_render = mocker.patch("mediaviewer.views.detail.render")
-
-        self.tv_path = Path.objects.create(
-            localpathstr="tv.local.path", remotepathstr="tv.remote.path", is_movie=False
-        )
-        self.tv_path.tvdb_id = None
-
-        self.tv_file = File.new("tv.file", self.tv_path)
-        self.tv_file.override_filename = "test str"
-        self.tv_file.override_season = "3"
-        self.tv_file.override_episode = "5"
-
-        mv_group = Group(name="MediaViewer")
-        mv_group.save()
-
-        self.user = UserSettings.new("test_user", "a@b.com", send_email=False)
-        settings = self.user.settings()
-        settings.force_password_change = False
-
-        self.request = mock.MagicMock(HttpRequest)
-        self.request.user = self.user
-
-    def test_no_comment(self):
-        expected_context = {
-            "file": self.tv_file,
-            "posterfile": self.tv_file.posterfile,
-            "comment": "",
-            "skip": self.tv_file.skip,
-            "finished": self.tv_file.finished,
-            "LOCAL_IP": LOCAL_IP,
-            "BANGUP_IP": BANGUP_IP,
-            "viewed": False,
-            "can_download": True,
-            "file_size": None,
-            "active_page": "tvshows",
-            "title": "Tv Local Path",
-            "displayName": "tv.file",
-        }
-        expected = self.mock_render.return_value
-        actual = filesdetail(self.request, self.tv_file.id)
-
-        assert expected == actual
-        self.mock_setSiteWideContext.assert_called_once_with(
-            expected_context, self.request
-        )
-        self.mock_render.assert_called_once_with(
-            self.request, "mediaviewer/filesdetail.html", expected_context
-        )
-
-    def test_comment(self):
-        usercomment = UserComment()
-        usercomment.file = self.tv_file
-        usercomment.user = self.user
-        usercomment.viewed = True
-        usercomment.comment = "test_comment"
-        usercomment.save()
-
-        expected_context = {
-            "file": self.tv_file,
-            "posterfile": self.tv_file.posterfile,
-            "comment": "test_comment",
-            "skip": self.tv_file.skip,
-            "finished": self.tv_file.finished,
-            "LOCAL_IP": LOCAL_IP,
-            "BANGUP_IP": BANGUP_IP,
-            "viewed": True,
-            "can_download": True,
-            "file_size": None,
-            "active_page": "tvshows",
-            "title": "Tv Local Path",
-            "displayName": "tv.file",
-        }
-        expected = self.mock_render.return_value
-        actual = filesdetail(self.request, self.tv_file.id)
-
-        assert expected == actual
-        self.mock_setSiteWideContext.assert_called_once_with(
-            expected_context, self.request
-        )
-        self.mock_render.assert_called_once_with(
-            self.request, "mediaviewer/filesdetail.html", expected_context
-        )
-
-
-@pytest.mark.django_db
 class TestAjaxViewed:
     @pytest.fixture(autouse=True)
-    def setUp(self, mocker):
+    def setUp(self,
+              mocker,
+              create_user,
+              create_tv_media_file):
         self.mock_HttpResponse = mocker.patch("mediaviewer.views.detail.HttpResponse")
 
         self.mock_dumps = mocker.patch("mediaviewer.views.detail.json.dumps")
 
-        self.tv_path = Path.objects.create(
-            localpathstr="tv.local.path", remotepathstr="tv.remote.path", is_movie=False
-        )
-        self.tv_path.tvdb_id = None
-
-        self.tv_file = File.new("tv.file", self.tv_path)
-        self.tv_file.override_filename = "test str"
-        self.tv_file.override_season = "3"
-        self.tv_file.override_episode = "5"
+        self.tv_file = create_tv_media_file()
 
         mv_group = Group(name="MediaViewer")
         mv_group.save()
 
-        self.user = UserSettings.new("test_user", "a@b.com", send_email=False)
-        self.user.settings().force_password_change = False
+        self.user = create_user()
 
         self.request = mock.MagicMock(HttpRequest)
         self.request.user = self.user
@@ -312,7 +204,10 @@ class TestAjaxViewed:
 @pytest.mark.django_db
 class TestAjaxDownloadButton:
     @pytest.fixture(autouse=True)
-    def setUp(self, mocker):
+    def setUp(self,
+              mocker,
+              create_user,
+              create_tv_media_file):
         self.mock_HttpResponse = mocker.patch("mediaviewer.views.detail.HttpResponse")
 
         self.mock_dumps = mocker.patch("mediaviewer.views.detail.json.dumps")
@@ -325,21 +220,12 @@ class TestAjaxDownloadButton:
             "mediaviewer.views.detail.File.downloadLink"
         )
 
-        self.tv_path = Path.objects.create(
-            localpathstr="tv.local.path", remotepathstr="tv.remote.path", is_movie=False
-        )
-        self.tv_path.tvdb_id = None
-
-        self.tv_file = File.new("tv.file", self.tv_path)
-        self.tv_file.override_filename = "test str"
-        self.tv_file.override_season = "3"
-        self.tv_file.override_episode = "5"
+        self.tv_file = create_tv_media_file()
 
         mv_group = Group(name="MediaViewer")
         mv_group.save()
 
-        self.user = UserSettings.new("test_user", "a@b.com", send_email=False)
-        self.user.settings().force_password_change = False
+        self.user = create_user()
 
         self.request = mock.MagicMock(HttpRequest)
         self.request.user = self.user
@@ -383,58 +269,12 @@ class TestAjaxDownloadButton:
 
 
 @pytest.mark.django_db
-class TestDownloadlink:
-    @pytest.fixture(autouse=True)
-    def setUp(self, mocker):
-        self.mock_downloadLink = mocker.patch(
-            "mediaviewer.views.detail.File.downloadLink"
-        )
-
-        self.mock_downloadtoken_new = mocker.patch(
-            "mediaviewer.views.detail.DownloadToken.new"
-        )
-
-        self.mock_redirect = mocker.patch("mediaviewer.views.detail.redirect")
-
-        self.tv_path = Path.objects.create(
-            localpathstr="tv.local.path", remotepathstr="tv.remote.path", is_movie=False
-        )
-        self.tv_path.tvdb_id = None
-
-        self.tv_file = File.new("tv.file", self.tv_path)
-        self.tv_file.override_filename = "test str"
-        self.tv_file.override_season = "3"
-        self.tv_file.override_episode = "5"
-
-        mv_group = Group(name="MediaViewer")
-        mv_group.save()
-
-        self.user = UserSettings.new("test_user", "a@b.com", send_email=False)
-        self.user.settings().force_password_change = False
-
-        self.request = mock.MagicMock(HttpRequest)
-        self.request.user = self.user
-
-    def test_no_file(self):
-        with pytest.raises(Http404):
-            downloadlink(self.request, 0)
-
-    def test_valid(self):
-        expected = self.mock_redirect.return_value
-        actual = downloadlink(self.request, self.tv_file.id)
-
-        assert expected == actual
-        self.mock_downloadtoken_new.assert_called_once_with(self.user, self.tv_file)
-        self.mock_downloadLink.assert_called_once_with(
-            self.user, self.mock_downloadtoken_new.return_value.guid
-        )
-        self.mock_redirect.assert_called_once_with(self.mock_downloadLink.return_value)
-
-
-@pytest.mark.django_db
 class TestAutoPlayDownloadLink:
     @pytest.fixture(autouse=True)
-    def setUp(self, mocker):
+    def setUp(self,
+              mocker,
+              create_user,
+              create_tv_media_file):
         self.mock_autoplayDownloadLink = mocker.patch(
             "mediaviewer.views.detail.File.autoplayDownloadLink"
         )
@@ -445,21 +285,12 @@ class TestAutoPlayDownloadLink:
 
         self.mock_redirect = mocker.patch("mediaviewer.views.detail.redirect")
 
-        self.tv_path = Path.objects.create(
-            localpathstr="tv.local.path", remotepathstr="tv.remote.path", is_movie=False
-        )
-        self.tv_path.tvdb_id = None
-
-        self.tv_file = File.new("tv.file", self.tv_path)
-        self.tv_file.override_filename = "test str"
-        self.tv_file.override_season = "3"
-        self.tv_file.override_episode = "5"
+        self.tv_file = create_tv_media_file()
 
         mv_group = Group(name="MediaViewer")
         mv_group.save()
 
-        self.user = UserSettings.new("test_user", "a@b.com", send_email=False)
-        self.user.settings().force_password_change = False
+        self.user = create_user()
 
         self.request = mock.MagicMock(HttpRequest)
         self.request.user = self.user
