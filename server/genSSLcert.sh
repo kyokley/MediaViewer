36ff9ddba4dab76d7843ea7d36084fec2689febc
Kevin Yokley <kyokley@mars>
2024-09-26 21:05:57 -0500

    Remove server dir

--- server/genSSLcert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-openssl genrsa -aes256 -out server.key 4096
-openssl req -sha256 -new -key server.key -out server.csr
-cp server.key server.key.org
-openssl rsa -in server.key.org -out server.key
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
-
