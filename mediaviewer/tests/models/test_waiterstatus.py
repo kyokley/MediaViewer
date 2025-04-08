18456ff4f4fe6683958eceeab62c493796a16e90
Kevin Yokley <kyokley@mars>
2025-04-07 19:04:13 -0500

    Limit WaiterStatus query

--- mediaviewer/tests/models/test_waiterstatus.py
+++ mediaviewer/tests/models/test_waiterstatus.py
@@ -1,21 +1,26 @@
-from django.test import TestCase
+import pytest
 
 from mediaviewer.models.waiterstatus import WaiterStatus
 
 
-class TestNew(TestCase):
+@pytest.mark.django_db
+class TestNew:
     def test_new(self):
         ws = WaiterStatus.new(True, "reason")
 
-        self.assertEqual(ws.status, True)
-        self.assertEqual(ws.failureReason, "reason")
+        assert ws.status is True
+        assert ws.failureReason == "reason"
 
 
-class TestGetLastStatus(TestCase):
-    def setUp(self):
+@pytest.mark.django_db
+class TestGetLastStatus:
+    def test_getLastStatus(self):
         self.ws1 = WaiterStatus.new(False, "reason1")
         self.ws2 = WaiterStatus.new(False, "reason2")
 
-    def test_getLastStatus(self):
         ws = WaiterStatus.getLastStatus()
-        self.assertEqual(ws, self.ws2)
+        assert ws == self.ws2
+
+    def test_empty(self):
+        ws = WaiterStatus.getLastStatus()
+        assert ws is None
