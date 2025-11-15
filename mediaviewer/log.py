31e1fc8d8d2a21a3b8f64b027bd50bbc8852b16a
Kevin Yokley <kyokley2@gmail.com>
2025-11-15 08:31:11 -0600

    Use StreamHandler for logging

--- mediaviewer/log.py
+++ mediaviewer/log.py
@@ -1,5 +1,5 @@
 import logging
-from logging.handlers import RotatingFileHandler
+from logging.handlers import StreamHandler
 
 from django.conf import settings
 
@@ -18,17 +18,18 @@ class LogFile(object):
         if not cls.logger:
             log = logging.getLogger("mediaviewer")
             log.setLevel(settings.LOG_LEVEL)
-            rfh = RotatingFileHandler(
-                settings.LOG_FILE_NAME,
-                mode="a",
-                maxBytes=10000000,
-                backupCount=10,
-            )
+            # fh = RotatingFileHandler(
+            #     settings.LOG_FILE_NAME,
+            #     mode="a",
+            #     maxBytes=10000000,
+            #     backupCount=10,
+            # )
+            fh = StreamHandler()
             formatter = logging.Formatter(
                 "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
             )
-            rfh.setFormatter(formatter)
-            log.addHandler(rfh)
+            fh.setFormatter(formatter)
+            log.addHandler(fh)
             cls.logger = log
 
         return cls.logger
