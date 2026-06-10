1d4661f5e0c9eeaaa54ab39a36a705f35c554cd7
Kevin Yokley <kyokley@mars>
2026-06-09 22:03:45 -0500

    ✨ feat(api-key)(api): add MCP media file viewset with TV filtering
    - replace IsStaffOrReadOnly with IsStaffReadOnlyOrCheckAPIKey for API key auth
    - add permission class to MediaFileAutoplayViewSet
    - switch from JsonResponse to DRF Response for consistency
    - add MCPMediaFileViewSet with tv_id query param filtering

--- mediaviewer/api/media_file_viewset.py
+++ mediaviewer/api/media_file_viewset.py
@@ -1,24 +1,42 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework import serializers
 
-from django.http import JsonResponse
+from rest_framework.response import Response
 from mediaviewer.models import MediaFile
 from mediaviewer.models.downloadtoken import DownloadToken
-from mediaviewer.api.permissions import IsStaffOrReadOnly
+from mediaviewer.api.permissions import IsStaffReadOnlyOrCheckAPIKey
 from mediaviewer.api.serializers import MediaFileSerializer
 
 
 class MediaFileViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsStaffOrReadOnly,)
+    permission_classes = (IsStaffReadOnlyOrCheckAPIKey,)
     queryset = MediaFile.objects.order_by("id")
     serializer_class = MediaFileSerializer
 
 
 class MediaFileAutoplayViewSet(viewsets.ViewSet):
+    permission_classes = (IsStaffReadOnlyOrCheckAPIKey,)
+
     def retrieve(self, request, pk=None):
         user = request.user
         mf = get_object_or_404(MediaFile, pk=pk)
         dt = DownloadToken.objects.from_media_file(user, mf)
 
         downloadlink = mf.autoplayDownloadLink(dt.guid)
-        return JsonResponse({"link": downloadlink})
+        return Response({"link": downloadlink})
+
+
+class MCPMediaFileViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsStaffReadOnlyOrCheckAPIKey,)
+    queryset = MediaFile.objects.all()
+    serializer_class = MediaFileSerializer
+
+    def list(self, request):
+        if "tv_id" not in request.query_params:
+            raise serializers.ValidationError("'imdb_id' is a required argument")
+
+        tv_id = request.query_params["tv_id"]
+        mfs = MediaFile.objects.filter(tv=tv_id)
+        serializer = self.serializer_class(mfs, many=True)
+        return Response(serializer.data)
